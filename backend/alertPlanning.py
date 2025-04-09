d851512c9bb8eeebff689a0a4ef2ce7290f5d9c1
divijmuthu <divijmuthu@gmail.com>
2025-04-08 19:54:57 -0700

    added requirements.txt for libraries

--- backend/alertPlanning.py
+++ backend/alertPlanning.py
@@ -3,7 +3,6 @@ import pandas as pd
 from datetime import datetime
 from datetime import date
 from pymongo import MongoClient
-import ssl
 import certifi
 import os
 from dotenv import load_dotenv
@@ -14,7 +13,7 @@ load_dotenv()
 CONNECTION_STRING = os.getenv("CONNECTION_STRING")
 
 client = MongoClient(CONNECTION_STRING, 
-                     ssl=True,tlsCAFile=certifi.where(), tls = True, tlsAllowInvalidCertificates=True)
+                     ssl=True, tlsCAFile=certifi.where(), tls = True, tlsAllowInvalidCertificates=True)
 #https://www.geeksforgeeks.org/pymongoarrow-export-and-import-mongodb-data-to-pandas-dataframe-and-numpy/
 warnings = [] # list of ingredients we need to warn about, maybe timestamps for reminders
 warningTimeStamps = {} # possibly store previous order times as dict, Ingred: Time
