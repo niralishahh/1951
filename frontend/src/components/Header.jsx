84ee16c623b37c601bf3d8792f04e913a78e7944
compscibear <cadelewis2024@gmail.com>
2025-06-17 18:08:17 -0700

    created navbar without the log in and getting started buttons and fixed the mobile view dropdown

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,45 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/images/image_7__1_-removebg-preview 1.png';
 
 export default function Header() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <header>
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2 border-b">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2 border-b relative">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <a href="/" className="flex items-center">
                         <img src={logo} className="mr-4 h-12 sm:h-16" alt="1951 Logo" />
                     </a>
-                    <div className="flex items-center lg:order-2">
-                        <a href="#" className="text-black hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 ">Log in</a>
-                        <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Get started</a> 
-                        <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-black rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </button>
-                    </div>
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Inventory</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Recipes</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Notifications</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
+                    
+                    {/* Mobile menu button */}
+                    <button 
+                        onClick={toggleMobileMenu}
+                        type="button" 
+                        className="inline-flex items-center p-2 ml-1 text-sm text-black rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" 
+                        aria-controls="mobile-menu-2" 
+                        aria-expanded={isMobileMenuOpen}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        {!isMobileMenuOpen ? (
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+                            </svg>
+                        ) : (
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                            </svg>
+                        )}
+                    </button>
+                </div>
+                
+                {/* Mobile navigation menu */}
+                <div className={`lg:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu-2">
+                    <ul className="flex flex-col mt-4 font-medium border-t border-gray-200 pt-4">
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50">Inventory</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50">Recipes</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50">Notifications</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:bg-gray-50">Settings</a>
+                        </li>
+                    </ul>
+                </div>
+                
+                {/* Desktop navigation menu */}
+                <div className="hidden lg:flex justify-center items-center absolute top-1/2 left-[48%] transform -translate-x-1/2 -translate-y-1/2">
+                    <ul className="flex font-medium space-x-8">
+                        <li>
+                            <a href="#" className="text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:p-0">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:p-0">Inventory</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:p-0">Recipes</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:p-0">Notifications</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-black hover:bg-gray-50 lg:hover:bg-transparent lg:hover:text-primary-700 lg:p-0">Settings</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
